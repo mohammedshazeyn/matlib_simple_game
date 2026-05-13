e0e0365efaec24ddd9151a72b7bd32bb99f9ac6e
mohammed shazeyn <mohammedshazeynshaz@gmail.com>
2026-05-13 12:24:21 +0530

    Add files via upload
    Made using Python basics like input(), variables, and f-strings. The game takes user input and creates a funny custom story interactively.

---
+++ matblit game 7th .py
@@ -0,0 +1,19 @@
+# Matlibs game 
+# word game where you create a stoty.
+# by felling in balnks with the random.
+
+game = input("what game you saw in market (WRITE ONLY GAME NAME): ")
+expression = input("what's your expression that that time? (WRITE ONLY EXPRESSION ):")
+mom = input("wahts your mom saying at that time? :")
+price_game = input("what's the prize of te game you saw, and you want to buy? (WRITE THE PRIZE OF THE GAME - ONLY PRIZE) :")
+
+
+print ("------------------------------------------------------------------------------------------------------------------")
+
+print("matblit story")
+#interviewer
+
+print(f"i was walking with my mom to my aunt house, suddenly i was {game} game")
+print(f"i was {expression} because my mom doesn't wanna buy me a game")
+print(f"my mom was saying -- {mom}")
+print(f"because the prize you the game was -- {price_game}$ ")
